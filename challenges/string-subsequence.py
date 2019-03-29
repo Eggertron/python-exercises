10f499cf418180a69a1d414ab7592421b9177148
Edgar Han <edgarh@vt.edu>
2019-03-28 22:57:44 -0400

    added a new challenge. strings subsequence.

---
+++ challenges/string-subsequence.py
@@ -0,0 +1,11 @@
+######################################
+# String Subsequence
+# Given 2 strings of characters,
+# find the longest subsequnce that
+# exists in both strings
+######################################
+
+import time
+
+
+
